9cffabdf9cf560d98e287608bb2fd0f23c10bbfb
Mujju <sayed.mujassama@gmail.com>
2020-04-03 17:38:58 -0400

    Added input and buttobn

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
-import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h3>Grocery StockUp</h3>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: ""
+    };
+  }
+  addData = e => {
+    this.setState({
+      data: e.target.value
+    });
+  };
+  add = () => {
+    console.log(`Data: ${this.state.data}`);
+  };
+  render() {
+    return (
+      <div>
+        <input type="text" onChange={this.addData} />
+        <button onClick={this.add}>Add</button>
+      </div>
+    );
+  }
 }
-
 export default App;
